98649a32974c5371e6101538fd55711fc170f033
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-09-06 17:45:05 +0530

    soon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+  return (
+    <div className="bg-black h-screen w-screen flex justify-center items-center">
+      <div className=" border-zinc-400 font-serif text-zinc-100 text-xl">coming soon.</div>
+      
+
+
+    </div>
+  )
+}
+
+export default App
