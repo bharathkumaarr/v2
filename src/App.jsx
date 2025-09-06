44160e0a5a8b59e503a3bcd645d981f1b7fa6a2e
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-09-06 18:50:12 +0530

    essential

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   
   return (
     <div className="bg-black h-screen w-screen flex justify-center items-center">
-      <div className=" border-zinc-400 font-serif text-zinc-100 text-xl">coming soon.</div>
+      <div className=" border-zinc-400 font-serif text-zinc-100 text-xl selection:bg-zinc-100 selection:text-black">coming soon.</div>
       
 
 
