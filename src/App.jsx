8a7ed9fb6910bf35275d508d1dee0c16d95b3f2e
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-09-07 20:00:27 +0530

    resume pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { Route, Routes, Link } from 'react-router-dom'
+import Resume from './components/Resume'
+
+
 function App() {
   
   return (
-    <div className="bg-black h-screen w-screen flex justify-center items-center">
-      <div className=" border-zinc-400 font-serif text-zinc-100 text-xl selection:bg-zinc-100 selection:text-black">coming soon.</div>
+    <div className="bg-black h-screen w-screen flex justify-center items-center flex-col">
       
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex flex-col justify-center items-center">
+              <div className="border-zinc-400 font-awesome-serif text-zinc-100 text-2xl selection:bg-zinc-100 selection:text-black mb-2">
+                portfolio website coming soon.
+              </div>
 
-
+              {/* Link works like an <a> tag but for SPA */}
+              <Link
+                to="/resume"
+                className="text-zinc-100 font-awesome-serif rounded hover:text-zinc-300"
+              >
+                View Resume &rarr;
+              </Link>
+            </div>
+          }
+        />
+        <Route path='/resume' element={<Resume />}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Resume.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Resume() {
+  return (
+    <div>
+        <iframe
+        src='/Resume.pdf'
+        className='w-screen h-screen'
+        title='Resume'
+        ></iframe>
+    </div>
+  )
+}
+
+export default Resume
